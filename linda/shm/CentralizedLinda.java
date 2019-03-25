16d401bb80e399a4743e765c249db392349d4e33
purerstamp <edouard@the-redunicorn.fr>
2019-03-25 14:41:46 +0100

    Adding tuplespace

--- linda/shm/CentralizedLinda.java
+++ linda/shm/CentralizedLinda.java
@@ -1,5 +1,7 @@
 package linda.shm;
 
+import java.util.ArrayList;
+
 import linda.Callback;
 import linda.Linda;
 import linda.Tuple;
@@ -10,6 +12,6 @@ public class CentralizedLinda implements Linda {
     public CentralizedLinda() {
     }
 
-    // TO BE COMPLETED
+    ArrayList<Tuple> tuplespace;
 
 }
