6b39e46ab72f0be7d7d0ceb38114d2f4c38cf5a6
Bastien Pelissier <bast.pelissier@gmail.com>
2019-03-25 17:57:16 +0100

    start

--- linda/shm/CentralizedLinda.java
+++ linda/shm/CentralizedLinda.java
@@ -1,6 +1,6 @@
 package linda.shm;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import linda.Callback;
 import linda.Linda;
@@ -12,6 +12,80 @@ public class CentralizedLinda implements Linda {
     public CentralizedLinda() {
     }
 
-    ArrayList<Tuple> tuplespace;
+    ArrayList<Tuple> tuplespace = new ArrayList<Tuple>();
 
+	@Override
+	public void write(Tuple t) {
+		synchronized (this) {
+			tuplespace.add(t);
+			this.notify();
+		}
+	}
+
+	@Override
+	public Tuple take(Tuple template) {
+		
+		Tuple t_temp = read(template);
+		tuplespace.remove(t_temp);
+		return t_temp;
+	}
+
+	@Override
+	public Tuple read(Tuple template) {
+
+		synchronized (this) {
+			do {
+				for (Tuple t : tuplespace) {
+					if (t.matches(template)) {
+						return t;
+					}
+				}
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} while (true);
+		}
+	}
+
+	@Override
+	public Tuple tryTake(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tuple tryRead(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Tuple> takeAll(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Tuple> readAll(Tuple template) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void eventRegister(eventMode mode, eventTiming timing, Tuple template, Callback callback) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void debug(String prefix) {
+		System.out.println("DEBUG " + "id : " + prefix.toString());
+		Iterator<Tuple> iterator = tuplespace.iterator();
+		while (iterator.hasNext()) {
+			Tuple t = iterator.next();
+			System.out.println(t);
+		}
+	}
 }
