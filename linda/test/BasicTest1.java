d1e99eb7916cdf5a3c24fb860b3bf7be320edef6
purerstamp <edouard@the-redunicorn.fr>
2019-04-17 15:34:02 +0200

    renommage tests

--- linda/test/BasicTest1.java
+++ linda/test/BasicTest1.java
@@ -6,8 +6,8 @@ public class BasicTest1 {
 
     public static void main(String[] a) {
                 
-        final Linda linda = new linda.shm.CentralizedLinda();
-        // final Linda linda = new linda.server.LindaClient("//localhost:4000/aaa");
+        //final Linda linda = new linda.shm.CentralizedLinda();
+        final Linda linda = new linda.server.LindaClient("//localhost:1099/");
                 
         new Thread() {
             public void run() {
---
+++ linda/test/CustomTest9.java
@@ -0,0 +1,66 @@
+package linda.test;
+
+import linda.*;
+
+/**
+ * Test if take primitive is a blocking operation.
+ * Using LindaClient.
+ *
+ */
+public class CustomTest9 {
+
+	public static void main(String[] args) {
+
+		//final Linda linda = new linda.shm.CentralizedLinda();
+        final Linda linda = new linda.server.LindaClient("//localhost:1099/");
+
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(2);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				Tuple motif = new Tuple(Integer.class, Integer.class);
+				Tuple res1 = linda.take(motif);
+				System.out.println("(1) Result:" + res1);
+				Tuple res2 = linda.take(motif);
+				System.out.println("(1) Result:" + res2);
+				linda.debug("(1)");
+			}
+		}.start();
+
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				Tuple t1 = new Tuple(4, 5);
+                System.out.println("(2) write: " + t1);
+                linda.write(t1);
+				
+				linda.debug("(2)");
+			}
+		}.start();
+		
+		new Thread() {
+			public void run() {
+				try {
+					Thread.sleep(4000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				Tuple t1 = new Tuple(4, 5);
+                System.out.println("(3) write: " + t1);
+                linda.write(t1);
+				
+				linda.debug("(3)");
+			}
+		}.start();
+	}
+
+}
